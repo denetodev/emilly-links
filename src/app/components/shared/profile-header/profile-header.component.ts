4e219c420be2fea7259776bea4148ada2a7b4008
Torres-Neto <torresneto.tn@gmail.com>
2025-07-19 12:12:58 -0300

    ajuste no título principal, inclusão de vídeos e ajustes nos textos

--- src/app/components/shared/profile-header/profile-header.component.ts
+++ src/app/components/shared/profile-header/profile-header.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-profile-header',
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
   templateUrl: './profile-header.component.html',
   styleUrl: './profile-header.component.css',
 })
-export class ProfileHeaderComponent {}
+export class ProfileHeaderComponent implements OnInit {
+  randomVideo: string = '';
+
+  ngOnInit(): void {
+    // Array of video URLs
+    const videos = [
+      'assets/videos/emilly-profile-video-1.mp4',
+      'assets/videos/emilly-profile-video-2.mp4',
+      'assets/videos/emilly-profile-video-3.mp4',
+      'assets/videos/emilly-profile-video-4.mp4',
+    ];
+
+    const randomIndex = Math.floor(Math.random() * videos.length);
+    this.randomVideo = videos[randomIndex];
+  }
+}
